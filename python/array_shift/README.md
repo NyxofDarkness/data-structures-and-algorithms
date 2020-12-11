94d301d389f7d9338b3c7d8db16bfe3a35af2531
NyxofDarkness <ibelieve.as@gmail.com>
2020-12-10 22:58:52 -0800

    committing, must do midpoint tomorrow

---
+++ python/array_shift/README.md
@@ -0,0 +1,21 @@
+# Challenge Summary
+ 
+a challenge to shift some array
+ 
+## Challenge Description
+ 
+Insert and shift an array in middle at index
+ 
+## Approach & Efficiency
+ 
+BigO = O(n) array insertion
+Complexity is fair
+ 
+## Solution
+ 
+![Array Shift Whiteboard](python/assets/reverse-array.png)
+ 
+![Array Shift Code](python/array_shift/array_shift.py)
+ 
+Worked with [Alan Karlo Mangubat](https://github.com/kmangub)
+Worked with [Brandon Gonzalez](https://github.com/bjgman12https://github.com/bjgman12)
---
+++ python/array_shift/tests/test_array_shift.py
@@ -0,0 +1,26 @@
+from array_shift import __version__
+from array_shift.array_shift import array_length, insertShiftArray
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+ 
+# test = [3,4,5,6,7]
+ 
+# def array_length(test):
+#   return len(test)
+
+def test_array_length():
+    actual = array_length([3,4,5,6,7])
+    expected = 5
+    assert actual == expected
+
+# def test_midpoint():
+#   actual = midpoint( 2 % array_length(temp))
+#   expected = 1
+#   assert actual == expected
+
+def test_insertion():
+    actual = insertShiftArray([3,4,5,6,7], 2, 20)
+    expected = [3,4,20, 5, 6, 7]
+    assert actual == expected
