55d5efefa7c903bd9d1345fe4537e90e59df9e22
NyxofDarkness <ibelieve.as@gmail.com>
2020-12-20 22:30:50 -0800

    a nice start, two test passing, need to add more and make sure it returns a string correctly

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -4,14 +4,20 @@ class Node:
         self.value = value
         self.next = None
 
+    def __str__(self):
+        return f"{self.value}"
+
 class LinkedList:
     """
     Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
     """
 
-    def __init__(self, head, values=None):
+    def __init__(self, value=None):
         # initialization here
-        self.head = head
+        self.head = None 
+        if not value is None:
+            node = Node(value)
+            self.head = node
 
     def insert(self, value):
         node = Node(value)
@@ -20,4 +26,16 @@ class LinkedList:
             node.next = self.head
         self.head = node
 
+    def includes(self, value):
+        if self.value is not None:
+            return True
+        else:
+            return False
 
+    def __str__():
+        nodes = []
+        if self.value is not None:
+            nodes.append(self.value)
+        else:
+            print(nodes)
+            
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,18 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
 def test_import():
     assert LinkedList
+
+a = Node(4)
+b = LinkedList()
+
+def test_one():
+    assert (a.value == 4 and a.next is None)
+
+def test_two():
+    b.insert(8)
+    assert b.head.value == 8
+
+def test_three():
+    
