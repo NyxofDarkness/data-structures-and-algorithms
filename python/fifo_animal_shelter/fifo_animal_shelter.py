fb8c5f8e2dd6fb6a01031c3e4df2c64940b928ab
NyxofDarkness <ibelieve.as@gmail.com>
2021-01-13 23:36:38 -0800

    animal shelter started

---
+++ python/fifo_animal_shelter/fifo_animal_shelter.py
@@ -0,0 +1,31 @@
+class Node():
+    def __init__(self, cat, dog, next = None):
+        self.cat = cat
+        self.dog = dog
+        self.next = next
+
+class InvalidOperationError(BaseException):
+    pass
+
+class AnimalShelter():
+    """[holds dogs and cats using FIFO approach]
+    """
+    def __init__(self):
+        self.cat = Stack()
+        self.dog = Stack()
+
+    def enqueue(self, cat, dog):
+        """[add animal to shelter, either dog or cat]
+
+        Args:
+            dog ([type]):
+            cat ([type]): 
+        """
+        if self.cat.top.next == None:
+            return self.cat.top.value
+        elif:
+            self.dog.top.next == None
+            return self.dog.top.value
+        continue
+
+    
---
+++ python/tests/test_fifo_animal_shelter.py
@@ -0,0 +1,4 @@
+import pytest
+from fifo_animal_shelter.fifo_animal_shelter import Node, Stack, fifo_animal_shelter
+
+
