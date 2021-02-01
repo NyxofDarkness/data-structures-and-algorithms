ea775441d1b9ab97f39dcab4aad29824548ba56e
NyxofDarkness <ibelieve.as@gmail.com>
2021-01-31 20:43:33 -0800

    3 tests passing, 3 to go, need to clean up code

--- python/tests/test_tree.py
+++ python/tests/test_tree.py
@@ -1,6 +1,61 @@
+import pytest
+from tree.tree import Node, BinaryTree, BinarySearchTree
+
 # Can successfully instantiate an empty tree
+def test_tree():
+    tree = BinaryTree()
+    actual = tree.root
+    expected = None
+    assert actual == expected
+
 # Can successfully instantiate a tree with a single root node
-# Can successfully add a left child and right child to a single root node
+def test_single_root_tree():
+    tree = BinaryTree()
+    tree.root = Node(11)
+    actual = tree.root.value
+    expected = 11
+    assert actual == expected
+
+#  Can successfully add a left child and right child to a single root node
+def test_add_left_and_right():
+    tree = BinaryTree()
+    tree.root = Node(111)
+    tree.root.left = Node(11)
+    tree.root.right = Node(1)
+    actual = f"{tree.root.value}, {tree.left.value}, {tree.right.value}"
+    expected = "111, 11, 1"
+    assert actual == expected
+
 # Can successfully return a collection from a preorder traversal
+def test_tree_preorder():
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(11)
+    tree.root.right = Node(111)
+    tree.root.left.left = Node (1111)
+    tree.root.left.right = Node(11111)
+    actual = tree.pre_order()
+    expected = [1, 11, 1111, 11111, 111]
+    assert actual == expected
+
 # Can successfully return a collection from an inorder traversal
+def test_tree_inorder():
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(11)
+    tree.root.right = Node(2)
+    tree.root.left.left = Node(111)
+    tree.root.right.right = Node(22)
+    actual = tree.in_order()
+    assert actual == expected
+
 # Can successfully return a collection from a postorder traversal
+def test_tree_postorder():
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(11)
+    tree.root.right = Node(2)
+    tree.root.left.left = Node(111)
+    tree.root.left.right = Node(112)
+    actual = tree.post_order()
+    assert actual == expected
--- python/tree/tree.py
+++ python/tree/tree.py
@@ -1,31 +1,76 @@
 class Node:
-    def __init__(self, value=None, left=None, right=None):
+    def __init__(self, value = None, left = None, right = None):
         self.value = value
         self.left = left
         self.right = right
 
 class BinaryTree:
-    def __init__(self, root=None):
+    def __init__(self, root = None):
         self.root = root
+
 # Define these method 
     def pre_order(self):
-        pass
+        order = []
+        def traverse(root):
+            if not root:
+                return "Nothing here sadly"
+            order.append(root.value)
+            traverse(root.left)
+            traverse(root.right)
+        traverse(self.root)
+        return order
+        # def traverse(root):
+        #     if root.left is not None:
+        #         traverse(root.left.value)
+        #         print(root.left.value)
+        #     if root.right is not None:
+        #         traverse(root.right.value)
+        #     if not root:
+        #         return "Nothing here sadly"
+        # #     traverse(root.value)
+        # #     traverse(root.right)
+        # traverse(self.root)
 
     def in_order(self):
-        pass
+        in_order = []
+        def traverse(root):
+            # if not root:
+            #     return "Nothing here sadly"
+            # in_order.append(root.value)
+            # else:
+                if root.left:
+                    traverse(root.left)
+                in_order.append(root.value)
+                if root.right:
+                    traverse(root.right)
+        traverse(self.root)
+        return in_order
 
     def post_order (self):
-        pass
+        post_order = []
+        def traverse(root):
+            if not root:
+                return "Nothing here sadly"
+            traverse(root.left)
+            traverse(root.right)
+            post_order.append(root.value)
+        traverse(self.root)
+        return post_order
 
 # which returns an array of the values, ordered appropriately.
 # Any exceptions or errors that come from your code should be semantic, capturable errors. For example, rather than a default error thrown by your language, your code should raise/throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
 
 class BinarySearchTree(BinaryTree):
-    pass
-
-    def add(self, value):
-        pass
+    def contains(self, value):
+        def traverse(node):
+            
 # Define a method named add that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
 # Define a method named contains that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
+    def contains(self, val):
+        if self.root == val:
+            return True
+        elif self.right == None:
+            return False
+
 if __name__ == "__main__":
     pass
