7e5626ac649526d4f4550cb69671ee32ed53178b
NyxofDarkness <ibelieve.as@gmail.com>
2021-03-04 21:47:40 -0800

    tests and code pass, need to do blog

--- python/merge_sort/merge_sort.py
+++ python/merge_sort/merge_sort.py
@@ -30,7 +30,64 @@
 #     if i = left.length
 #        set remaining entries in arr to remaining values in right
 #     else
-#        set remaining entries in arr to remaining values in left
+#        set remaining entries in arr to remaining values in len
 
 def merge_sort(arr):
-    pass
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    i_left_index = 0
+    j_right_index = 0
+    k_result = []
+
+    while i_left_index < len(left) and j_right_index < len(right):
+        if left[i_left_index] < right[j_right_index]:
+            k_result.append(left[i_left_index])
+            i_left_index += 1
+        else:
+            k_result.append(right[j_right_index])
+            j_right_index += 1
+
+    k_result += left[i_left_index:]
+    k_result += right[j_right_index:]
+    return k_result
+
+
+# def merge(left, right):
+#     """Merge sort merging function."""
+
+#     left_index, right_index = 0, 0
+#     result = []
+#     while left_index < len(left) and right_index < len(right):
+#         if left[left_index] < right[right_index]:
+#             result.append(left[left_index])
+#             left_index += 1
+#         else:
+#             result.append(right[right_index])
+#             right_index += 1
+
+#     result += left[left_index:]
+#     result += right[right_index:]
+#     return result
+
+
+# def merge_sort(array):
+#     """Merge sort algorithm implementation."""
+
+#     if len(array) <= 1:  # base case
+#         return array
+
+#     # divide array in half and merge sort recursively
+#     half = len(array) // 2
+#     left = merge_sort(array[:half])
+#     right = merge_sort(array[half:])
+
+#     return merge(left, right)
+        
