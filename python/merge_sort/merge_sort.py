9278946d6d1b87303205313336fe32eae487b043
NyxofDarkness <ibelieve.as@gmail.com>
2021-03-08 20:36:17 -0800

    merge_sort BLOG done

--- python/merge_sort/merge_sort.py
+++ python/merge_sort/merge_sort.py
@@ -1,37 +1,3 @@
-# ALGORITHM Mergesort(arr)
-#     DECLARE n <-- arr.length
-           
-#     if n > 1
-#       DECLARE mid <-- n/2
-#       DECLARE left <-- arr[0...mid]
-#       DECLARE right <-- arr[mid...n]
-#       // sort the left side
-#       Mergesort(left)
-#       // sort the right side
-#       Mergesort(right)
-#       // merge the sorted left and right sides together
-#       Merge(left, right, arr)
-
-# ALGORITHM Merge(left, right, arr)
-#     DECLARE i <-- 0
-#     DECLARE j <-- 0
-#     DECLARE k <-- 0
-
-#     while i < left.length && j < right.length
-#         if left[i] <= right[j]
-#             arr[k] <-- left[i]
-#             i <-- i + 1
-#         else
-#             arr[k] <-- right[j]
-#             j <-- j + 1
-            
-#         k <-- k + 1
-
-#     if i = left.length
-#        set remaining entries in arr to remaining values in right
-#     else
-#        set remaining entries in arr to remaining values in len
-
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -58,36 +24,4 @@ def merge(left, right):
     k_result += left[i_left_index:]
     k_result += right[j_right_index:]
     return k_result
-
-
-# def merge(left, right):
-#     """Merge sort merging function."""
-
-#     left_index, right_index = 0, 0
-#     result = []
-#     while left_index < len(left) and right_index < len(right):
-#         if left[left_index] < right[right_index]:
-#             result.append(left[left_index])
-#             left_index += 1
-#         else:
-#             result.append(right[right_index])
-#             right_index += 1
-
-#     result += left[left_index:]
-#     result += right[right_index:]
-#     return result
-
-
-# def merge_sort(array):
-#     """Merge sort algorithm implementation."""
-
-#     if len(array) <= 1:  # base case
-#         return array
-
-#     # divide array in half and merge sort recursively
-#     half = len(array) // 2
-#     left = merge_sort(array[:half])
-#     right = merge_sort(array[half:])
-
-#     return merge(left, right)
         
