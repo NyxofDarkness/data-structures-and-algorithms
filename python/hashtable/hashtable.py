ded57fa4b9d74fa1fe5b74164308eecf5d3d34af
NyxofDarkness <ibelieve.as@gmail.com>
2021-03-15 21:39:50 -0700

    hashtable working, maybe

--- python/hashtable/hashtable.py
+++ python/hashtable/hashtable.py
@@ -1,29 +1,72 @@
-from python.linked_list.linked_list import Node, LinkedList
+from linked_list.linked_list import Node, LinkedList
 
 class HashTable:
 
     def __init__(self, size=1024):
-        self.size = sizeself._buckets = size * [None]
+        self.size = size
+        self._buckets = size * [None]
 
-    def hash(self, key):
+    def _hash(self, key):
         """[takes in an arbitrary key and returns an index in the collection.]
 
         Args:
             key ([type]): [returns index in collection]
         """
         sum = 0
+        for ch in key:
+            sum += ord(ch)
+
+        primed = sum * 19
+
+        index = primed % self.size
+
+        return index
     
-    def add():
+    def add(self, key, value):
         """[takes in both the key and value. This method should hash the key, and add the key and value pair to the table, handling collisions as needed.]
         """
-        pass
+        hashed_index = self._hash(key)
 
-    def get():
+        # if not self._buckets[hashed_index]:
+        #     self._buckets[hashed_index] = LinkedList()
+
+        # self._buckets[hashed_index].add((key, value))
+
+        if self.contains(key):
+            raise KeyError(f'hashtable contain key: {key} ')
+        else:
+            self._buckets[hashed_index] = LinkedList()
+
+        self._buckets[hashed_index].add((key, value))
+        #     self._bucket[hashed_index].insert((key, value))
+
+    def get(self, key):
         """[ takes in the key and returns the value from the table.]
         """
-        pass
+        bucket = self._bucket[self._hash(key)]
+        
+        if not self.contains(key):
+            raise ValueError(f'hashtable does not contain {key} ')
 
-    def contains(key):
+        current = bucket.head
+        while current:
+            if current.value[0] == key:
+                return current.value[1]
+            else:
+                current = current.next
+
+    def contains(self, key):
         """[takes in the key and returns a boolean, indicating if the key exists in the table already.]
         """
-        pass
+        bucket = self._bucket[self._hash(key)]
+        current = bucket.head
+        if current.value == None:
+            return False
+
+        while current:
+            if current.value[0] == key:
+                return True
+            else:
+                current = current.next
+
+        return False
--- python/tests/test_hashtable.py
+++ python/tests/test_hashtable.py
@@ -4,3 +4,38 @@
 # Successfully handle a collision within the hashtable
 # Successfully retrieve a value from a bucket within the hashtable that has a collision
 # Successfully hash a key to an in-range value
+
+from hashtable.hashtable import HashTable
+
+def test_create():
+    hashtable = HashTable()
+    assert hashtable
+
+def test_hash_range():
+    ht = HashTable()
+    actual = ht._hash('vader')
+    assert 0 <= actual < ht.size
+
+def test_know_hash():
+    ht = HashTable()
+    first_run = ht._hash('Luke')
+    second_run = ht._hash('Luke')
+    assert first_run == second_run
+
+def test_anagram():
+    ht = HashTable()
+    first_run = ht._hash('school master')
+    second_run = ht._hash('the classroom')
+    assert first_run == second_run
+
+def test_anagram():
+    ht = HashTable()
+    first_run = ht._hash('school master')
+    second_run = ht._hash('this should not work')
+    assert first_run != second_run
+
+def test_add_same():
+    ht = HashTable()
+    first_run = ht._hash('Luke')
+    index = 451
+    assert first_run == index
