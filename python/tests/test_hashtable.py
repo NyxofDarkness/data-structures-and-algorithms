a0dc70dca112362236cfd9772b0cdc398a81d2ca
NyxofDarkness <ibelieve.as@gmail.com>
2021-03-17 19:29:29 -0700

    hashtable fixed up

--- python/tests/test_hashtable.py
+++ python/tests/test_hashtable.py
@@ -1,16 +1,10 @@
-# Adding a key/value to your hashtable results in the value being in the data structure
-# Retrieving based on a key returns the value stored
-# Successfully returns null for a key that does not exist in the hashtable
-# Successfully handle a collision within the hashtable
-# Successfully retrieve a value from a bucket within the hashtable that has a collision
-# Successfully hash a key to an in-range value
-
 from hashtable.hashtable import HashTable
 
 def test_create():
     hashtable = HashTable()
     assert hashtable
 
+# Successfully hash a key to an in-range value
 def test_hash_range():
     ht = HashTable()
     actual = ht._hash('vader')
@@ -39,3 +33,34 @@ def test_add_same():
     first_run = ht._hash('Luke')
     index = 451
     assert first_run == index
+
+# Adding a key/value to your hashtable results in the value being in the data structure
+def test_add():
+    ht = HashTable()
+    ht.add('milk', 'cookies')
+    actual = ht.contains('milk')
+    expected = True
+    assert actual == expected
+
+# Successfully handle a collision within the hashtable
+# def test_add_same():
+#     ht = HashTable()
+#     ht.add('milk', 'cookies')
+#     actual = ht.add('milk', 'cookies')
+#     expected = 
+#     assert actual == expected
+
+# Retrieving based on a key returns the value stored
+def test_get():
+    ht = HashTable()
+    ht.add('milk', 'coookies')
+    actual = ht.get('milk')
+    expected = 'coookies'
+    assert actual == expected
+
+# Successfully returns null for a key that does not exist in the hashtable
+def test_contains():
+    ht = HashTable()
+    actual = ht.contains('milk')
+    expected = False
+    assert actual == expected
